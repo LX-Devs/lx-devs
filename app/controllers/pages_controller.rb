f53af313ca58c35b92322e27446fc72bcf20faed
Carlos Silva <cucmaluco@gmail.com>
2017-03-25 18:04:07 +0000

    added stupid weather widget. just because i can

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @users = User.all
     @developers = Developer.all
     @projects = Project.where.not(latitude: nil, longitude: nil)
     @hash = Gmaps4rails.build_markers(@projects) do |project, marker|
